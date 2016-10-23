0ad52a078daea99ab4d64eb47dcb000f91e67eff
Adriel Café <me@adriel.cafe>
2016-10-23 16:23:21 -0300

    Unregistering bus

--- app/src/main/kotlin/cafe/adriel/vanhackathon/shopify/readnbuy/view/ui/base/BaseActivity.kt
+++ app/src/main/kotlin/cafe/adriel/vanhackathon/shopify/readnbuy/view/ui/base/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.view.LayoutInflaterCompat
 import android.support.v7.app.AppCompatActivity
+import com.eightbitlab.rxbus.Bus
 import com.mikepenz.iconics.context.IconicsLayoutInflater
 import com.tsengvn.typekit.TypekitContextWrapper
 
@@ -16,6 +17,11 @@ abstract class BaseActivity: AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(TypekitContextWrapper.wrap(newBase))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Bus.unregister(this)
     }
 
 }
--- app/src/main/kotlin/cafe/adriel/vanhackathon/shopify/readnbuy/view/ui/base/BaseFragment.kt
+++ app/src/main/kotlin/cafe/adriel/vanhackathon/shopify/readnbuy/view/ui/base/BaseFragment.kt
@@ -1,7 +1,13 @@
 package cafe.adriel.voxrecorder.view.ui.base
 
 import android.support.v4.app.Fragment
+import com.eightbitlab.rxbus.Bus
 
 abstract class BaseFragment: Fragment() {
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Bus.unregister(this)
+    }
 
 }
